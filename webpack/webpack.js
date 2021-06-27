7365763236a39491451451cfd0e74bef79f2427b
吴群 <sky@wuqundeMacBook-Pro.local>
2021-06-27 15:39:21 +0800

    add webpack wheel

---
+++ webpack/webpack.js
@@ -0,0 +1,14 @@
+/**
+ * webpack编译依赖babel全家桶
+ * babel全家桶
+ * @babel/parser
+ * @babel/traverse
+ * @babel/core
+ */
+
+const fs = require('fs');
+const path = require('path');
+const parser = require('@babel/parser');
+
+
+
